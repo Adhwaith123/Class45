c3a0c0ddbdb0a5ced671a3993415d6fd33029ff3
Adhwaith123 <62431227+Adhwaith123@users.noreply.github.com>
2020-06-12 18:58:53 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 var king;
 var kinganimation,kingimage,kinganimation2,kinganimation3,kinganimation4;
 var ground;
+var ObstaclesGroup,Obstacles;
+
 //Animations and Images
 function preload(){
 kinganimation =loadAnimation("Images/MY GAME FILES/ninja1.png","Images/MY GAME FILES/ninja2.png","Images/MY GAME FILES/ninja3.png","Images/MY GAME FILES/ninja4.png","Images/MY GAME FILES/ninja5.png","Images/MY GAME FILES/ninja6.png");
@@ -13,6 +15,7 @@ function setup() {
   createCanvas(1600,600);
   king = createSprite(200, 500, 50, 50);
   ground=createSprite(600,590,2000,10);
+
   king.addImage("king",kingimage);
   king.scale =1.5;
 }
@@ -20,7 +23,7 @@ function setup() {
 function draw() {
   background("white"); 
   edges = createEdgeSprites();
-
+// Obstacles.velocityX = -10;
   // if(keyDown("space") && trex.y >= 359){
   //   trex.velocityY = -12 ;
   //   playSound("jump.mp3");
@@ -29,28 +32,27 @@ function draw() {
   // Moving of the king
   if(keyWentDown(RIGHT_ARROW)){
     king.addAnimation("king",kinganimation);
-    king.velocityX = 10;
+    // king.velocityX = 10;
 king.scale =0.9;
   }
 
   if(keyWentUp(RIGHT_ARROW)){
-     king.velocityX = 0;
+    //  king.velocityX = 0;
     king.addImage("king",kingimage);
     king.scale = 1.5;
   } 
   //  Jumping of the king
-//   if(keyWentDown(UP_ARROW)){ 
-//     king.velocityY = -12;
-//     king.addAnimation("king",kinganimation2);
-//  king.scale =1.5;
- 
-//   }
-  if(keyDown(UP_ARROW)&& king.y >= 491){
+  if(keyWentDown(UP_ARROW)&& king.collide(ground)){ 
     king.velocityY = -12;
-    // king.velocityY = king.velocityY +  0.5; 
-    king.addImage("king",kingimage);
-    king.scale = 1.5;
+    king.addAnimation("king",kinganimation2);
+ king.scale =1.5;
+ 
   }
+
+if(keyWentUp(UP_ARROW) && king.collide(ground)){
+  king.addImage("king",kingimage);
+  king.scale = 1.5;
+}
   console.log(king.y);
   //Attacking of the king
   if(keyWentDown("space")){
@@ -76,5 +78,13 @@ if(keyWentUp("space")){
 
  //Gravity 
  king.velocityY = king.velocityY +  0.7; 
+ spawnObstacles();
   drawSprites();
+}
+
+function spawnObstacles(){
+  if(World.frameCount % 60===0){
+    Obstacles=createSprite(1500,560,60,60);
+    Obstacles.velocityX = -10;
+  }
 }
